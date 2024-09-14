e702725de991a05eabbd84ac1a33272f48438ce1
chaixiang <973731820@qq.com>
2024-09-14 03:08:14 +0000

    1

--- kill-stream.sh
+++ kill-stream.sh
@@ -3,9 +3,12 @@ source util.sh
 str="arm-stream -i"
 num=$1
 
-
-run_cmd "kill \$(ps -ef | grep '$str $num' | grep -v grep | awk '{print \$2}')"
-
-run_cmd "docker exec android_$num start adbd"
-
-
+if [ "$num" == "-a" ]; then
+  for i in {0..3}; do
+    run_cmd "kill \$(ps -ef | grep '$str $i' | grep -v grep | awk '{print \$2}')"
+    run_cmd "docker exec android_$i start adbd"
+  done
+else
+  run_cmd "kill \$(ps -ef | grep '$str $num' | grep -v grep | awk '{print \$2}')"
+  run_cmd "docker exec android_$num start adbd"
+fi
